10c910e4c6817302a9ab65b7ec1c84c2b5173184
AndreRosa22 <andrefrancorosa@gmail.com>
2020-04-02 10:34:40 +0100

    Solution ex 5

--- MyFirstMethod/Program.cs
+++ MyFirstMethod/Program.cs
@@ -4,6 +4,10 @@ namespace MyFirstMethod
 {
     class Program
     {
+        /// <summary>
+        /// Main Method
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             CountTo10();
@@ -11,11 +15,18 @@ namespace MyFirstMethod
             CountToN(3);
         }
 
+        /// <summary>
+        /// Method that prints Numbers from 1 to 10
+        /// </summary>
         static void CountTo10()
         {
             CountToN(10);
         }
        
+        /// <summary>
+        /// Method that prints Numbers from 1 to n
+        /// </summary>
+        /// <param name="n">Maximum number to print</param>
         static void CountToN(int n)
         {
             for(int i = 1; i <= n; i++)
