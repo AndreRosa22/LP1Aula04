a40439c9acfc3a6d4b6e1d58306e31789528e91c
AndreRosa22 <andrefrancorosa@gmail.com>
2020-04-02 11:48:20 +0100

    Solution ex 7

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userNumber;
+            int n;
+            Console.WriteLine("Insira um número inteiro");
+            userNumber = Console.ReadLine();
+            n = Convert.ToInt32(userNumber);
+            Console.WriteLine("O número na posição " + n + " é: ");
+            Console.WriteLine(Fibonacci(n));
+        }
+
+        /// <summary>
+        /// Method that show the number in the n positon of febonacci sequence
+        /// </summary>
+        /// <param name="n">Position number</param>
+        /// <returns></returns>
+        static int Fibonacci(int n)
+        {
+            int result = 0;
+
+            if (n <= 2)
+            {
+                result = 1;
+            }
+
+            else
+                result = Fibonacci(n - 1) + Fibonacci(n - 2);
+            return result;
+        }
+    }
+}
