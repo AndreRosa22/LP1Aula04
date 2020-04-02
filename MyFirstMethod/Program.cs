53e3b6ba189d33f066ecd9e413ab9d6a5dcf68ba
AndreRosa22 <andrefrancorosa@gmail.com>
2020-04-02 10:20:36 +0100

    Solution ex 2

--- MyFirstMethod/Program.cs
+++ MyFirstMethod/Program.cs
@@ -6,7 +6,16 @@ namespace MyFirstMethod
     {
         static void Main(string[] args)
         {
-            
+            CountTo10();
+            CountTo10();
+        }
+
+        static void CountTo10()
+        {
+            for(int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
